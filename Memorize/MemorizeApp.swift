334b7120e978e56ab95836fbad8e54a9e1cae6c7
Pavel Senchenko <pavel.senchenko@gmail.com>
2021-04-06 15:22:08 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pavel Senchenko on 06.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
